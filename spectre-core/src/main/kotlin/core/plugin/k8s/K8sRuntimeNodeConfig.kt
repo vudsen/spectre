25a6fb805dec44ce33ca63808bc6cfc5abc5841d
vudsen <72915970+vudsen@users.noreply.github.com>
2025-12-03 10:24:10 +0800

    test: K8s 集成测试 (#3)
    * test: K8s 集成测试
    
    * fix: 工作流报错
    
    * ci: 构建时跳过测试

--- spectre-core/src/main/kotlin/core/plugin/k8s/K8sRuntimeNodeConfig.kt
+++ spectre-core/src/main/kotlin/core/plugin/k8s/K8sRuntimeNodeConfig.kt
@@ -3,8 +3,12 @@ package io.github.vudsen.spectre.core.plugin.k8s
 import io.github.vudsen.spectre.api.plugin.rnode.RuntimeNodeConfig
 
 data class K8sRuntimeNodeConfig(
-    var apiServerEndpoint: String = "",
-    var token: String = "",
+    var apiServerEndpoint: String,
+    var token: String,
     var spectreHome: String = "/opt/spectre",
     var insecure: Boolean = false
-) : RuntimeNodeConfig
+) : RuntimeNodeConfig {
+
+    constructor() : this("", "", "/opt/spectre", false)
+
+}
