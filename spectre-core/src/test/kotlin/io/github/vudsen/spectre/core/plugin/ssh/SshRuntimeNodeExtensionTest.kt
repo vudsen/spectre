f77aaa7e52dc02bf70cef45418f084e62aaaaa98
vudsen <72915970+vudsen@users.noreply.github.com>
2025-12-03 09:04:35 +0800

    test: Docker 集成测试 (#2)
    * feat: docker 集成测试
    
    * test: 完成 Docker 集成测试

--- spectre-core/src/test/kotlin/io/github/vudsen/spectre/core/plugin/ssh/SshRuntimeNodeExtensionTest.kt
+++ spectre-core/src/test/kotlin/io/github/vudsen/spectre/core/plugin/ssh/SshRuntimeNodeExtensionTest.kt
@@ -1,19 +1,27 @@
 package io.github.vudsen.spectre.core.plugin.ssh
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.github.dockerjava.api.DockerClient
+import io.github.vudsen.spectre.api.dto.JvmTreeNodeDTO
 import io.github.vudsen.spectre.api.service.ArthasExecutionService
 import io.github.vudsen.spectre.api.service.RuntimeNodeService
 import io.github.vudsen.spectre.repo.po.RuntimeNodePO
 import io.github.vudsen.spectre.test.AbstractSpectreTest
+import io.github.vudsen.spectre.test.Disposer
 import io.github.vudsen.spectre.test.TestConstant
 import io.github.vudsen.spectre.test.TestContainerUtils
 import io.github.vudsen.spectre.test.loop
 import org.junit.jupiter.api.Assertions
 import org.springframework.beans.factory.annotation.Autowired
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.startupcheck.IsRunningStartupCheckStrategy
+import org.testcontainers.containers.startupcheck.StartupCheckStrategy
+import org.testcontainers.containers.wait.strategy.DockerHealthcheckWaitStrategy
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper
+import org.testcontainers.utility.DockerImageName
 import kotlin.test.Test
 
 class SshRuntimeNodeExtensionTest : AbstractSpectreTest() {
@@ -24,54 +32,100 @@ class SshRuntimeNodeExtensionTest : AbstractSpectreTest() {
     @set:Autowired
     lateinit var arthasExecutionService: ArthasExecutionService
 
-    private fun insertSshRuntimeNode(container: GenericContainer<*>): Long {
+    @set:Autowired
+    lateinit var disposer: Disposer
+
+    companion object {
+        const val MATH_GAME = "math-game"
+    }
+
+
+    @Test
+    fun testDockerAttach() {
+        val runtimeNodeId = setupDockerAttachContainer()
+
+        val root = runtimeNodeService.expandRuntimeNodeTree(runtimeNodeId, null)
+        val dockerNode = root.get(0)
+
+        val dockerNodes = runtimeNodeService.expandRuntimeNodeTree(runtimeNodeId, dockerNode.id)
+
+        val mathGame = dockerNodes.find { node -> node.name.startsWith(MATH_GAME) }
+        Assertions.assertNotNull(mathGame)
+
+        testNodeAttach(runtimeNodeId, mathGame!!)
+    }
+
+    private fun setupDockerAttachContainer(): Long {
+        val container = GenericContainer(DockerImageName.parse(TestConstant.DOCKER_IMAGE_SSHD_WITH_DOCKER)).apply {
+            withExposedPorts(22)
+            withFileSystemBind("/var/run/docker.sock", "/var/run/docker.sock")
+        }
+        container.start()
+
+        // TODO 结束后删除容器
+        val result =
+            container.execInContainer("/usr/bin/docker", "run", "--name", MATH_GAME, "--rm", "-d", TestConstant.DOCKER_IMAGE_MATH_GAME)
+        if (result.exitCode != 0) {
+            Assertions.fail<Unit>("Failed to start docker run command: $result")
+        }
+        disposer.registerDispose {
+            container.execInContainer("/usr/bin/docker", "stop", MATH_GAME)
+        }
+
         val objectMapper = ObjectMapper()
         val conf = SshRuntimeNodeConfig(
+            SshRuntimeNodeConfig.Docker(true, "docker", null, null),
             null,
-            SshRuntimeNodeConfig.Local(true, "/opt/java"),
             container.host,
             container.firstMappedPort,
             "root",
             SshRuntimeNodeConfig.LoginPrincipal(
                 SshRuntimeNodeConfig.LoginType.PASSWORD,
-                "root",
+                "P@ssw0rd",
                 null,
                 null
             ),
             "/opt/spectre"
         )
-
-        return runtimeNodeService.saveRuntimeNode(RuntimeNodePO(
-            name = "Test Node",
-            pluginId = SshRuntimeNodeExtension.ID,
-            configuration = objectMapper.writeValueAsString(conf)
-        )).id!!
+        val runtimeNodeId = runtimeNodeService.saveRuntimeNode(
+            RuntimeNodePO(
+                name = "Test Node",
+                pluginId = SshRuntimeNodeExtension.ID,
+                configuration = objectMapper.writeValueAsString(conf)
+            )
+        ).id!!
+        return runtimeNodeId
     }
 
+
     @Test
-    fun ensureAttachEnvironmentReadyLocalAttach() {
-        val container = TestContainerUtils.createMathGameSshMachine()
-        val id = insertSshRuntimeNode(container)
+    fun testLocalAttach() {
+        val runtimeNodeId = setupContainerForLocal()
 
-        runtimeNodeService.getRuntimeNode(id)!!
 
-        val root = runtimeNodeService.expandRuntimeNodeTree(id, null)
+        runtimeNodeService.getRuntimeNode(runtimeNodeId)!!
+
+        val root = runtimeNodeService.expandRuntimeNodeTree(runtimeNodeId, null)
         val localeNode = root.get(0)
 
         val localJvms =
-            runtimeNodeService.expandRuntimeNodeTree(id, localeNode.id)
+            runtimeNodeService.expandRuntimeNodeTree(runtimeNodeId, localeNode.id)
 
         val mathGame = localJvms.get(0)
         Assertions.assertTrue(mathGame.isJvm)
 
 
+        testNodeAttach(runtimeNodeId, mathGame)
+    }
+
+    private fun testNodeAttach(runtimeNodeId: Long, mathGame: JvmTreeNodeDTO) {
         val status = loop(20) {
             val attachStatus = arthasExecutionService.requireAttach(
-                id,
+                runtimeNodeId,
                 mathGame.id,
                 TestConstant.TOOLCHAIN_BUNDLE_LATEST_ID
             )
-            attachStatus.error ?.let {
+            attachStatus.error?.let {
                 Assertions.fail<Unit>("Failed to attach: ${it.message}")
             }
             if (attachStatus.isReady) {
@@ -98,6 +152,37 @@ class SshRuntimeNodeExtensionTest : AbstractSpectreTest() {
                 Assertions.fail("Invalid type: ${result::class.java}")
             }
         }
+    }
+
+    private fun setupContainerForLocal(): Long {
+        val container = GenericContainer(DockerImageName.parse(TestConstant.DOCKER_IMAGE_SSH_WITH_MATH_GAME)).apply {
+            withExposedPorts(22)
+        }
+        container.start();
+
+        val objectMapper = ObjectMapper()
+        val conf = SshRuntimeNodeConfig(
+            null,
+            SshRuntimeNodeConfig.Local(true, "/opt/java"),
+            container.host,
+            container.firstMappedPort,
+            "root",
+            SshRuntimeNodeConfig.LoginPrincipal(
+                SshRuntimeNodeConfig.LoginType.PASSWORD,
+                "root",
+                null,
+                null
+            ),
+            "/opt/spectre"
+        )
+        val runtimeNodeId = runtimeNodeService.saveRuntimeNode(
+            RuntimeNodePO(
+                name = "Test Node",
+                pluginId = SshRuntimeNodeExtension.ID,
+                configuration = objectMapper.writeValueAsString(conf)
+            )
+        ).id!!
+        return runtimeNodeId
     }
 
     private fun checkScResult(node: ArrayNode) {
