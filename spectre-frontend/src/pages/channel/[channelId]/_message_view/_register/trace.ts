5bfcdd43cc0fc0c9fc4aa97fe8cbb8cf8c2e1aaf
vudsen <xu2237803016@outlook.com>
2025-12-31 11:02:13 +0800

    trace 视图

---
+++ spectre-frontend/src/pages/channel/[channelId]/_message_view/_register/trace.ts
@@ -0,0 +1,12 @@
+import { registerMessageView } from '@/pages/channel/[channelId]/_message_view/factory.ts'
+import TraceMessageDetail from '@/pages/channel/[channelId]/_message_view/_component/TraceMessageDetail.tsx'
+
+registerMessageView({
+  type: 'trace',
+  detailComponent: TraceMessageDetail,
+  display: () => ({
+    color: 'success',
+    name: `查看链路调用`,
+    tag: 'Trace',
+  }),
+})
